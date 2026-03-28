96c4ea869af18ea8f2300aa10dd1d23623d05915
VelonacepsCalyxEggs <ivankuvaev777@gmail.com>
2026-03-28 20:40:05 +0300

    Fix a typo.

--- source/Classes/PlayerExtensions.cs
+++ source/Classes/PlayerExtensions.cs
@@ -7,10 +7,6 @@ public static class PlayerExtensions
     {
         return (item as CustomQueueTrackItem);
     }
-    public static CustomQueueTrackItem? GetTrackCustomData(this TrackReference item)
-    {
-        return (item as CustomQueueTrackItem);
-    }
     public static bool TryGetCustomData(this LavalinkPlayer player, out CustomQueueTrackItem? data)
     {
         if (player.CurrentItem is CustomQueueTrackItem customItem)
